2664c9096f2ae2efc23aa3fd910a6a8b86969366
nerv617 <nerv617@gmail.com>
2019-04-14 00:37:53 -0400

    style

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const fs = require("fs");
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/static"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/static/index.html");
 });
